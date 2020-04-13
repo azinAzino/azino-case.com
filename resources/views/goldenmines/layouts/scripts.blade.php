f57ac76583e707dac3b13aedd91fa4846ba289d9
Maxim <maxim@MacBook-Pro-Maxim.local>
2020-04-13 16:38:23 +0600

    M

--- resources/views/goldenmines/layouts/scripts.blade.php
+++ resources/views/goldenmines/layouts/scripts.blade.php
@@ -4,5 +4,5 @@
  <div style="display:noned">
  <a href="//showstreams.tv/"><img src="//www.free-kassa.ru/img/fk_btn/14.png" title="Бесплатный видеохостинг"></a></div>
  -->
-<script src="http://azino-case.loc:3000/socket.io/socket.io.js"></script>
+<script src="http://azino-case.com:3000/socket.io/socket.io.js"></script>
 <script data-cfasync="false" src="/cdn-cgi/scripts/af2821b0/cloudflare-static/email-decode.min.js"></script>
