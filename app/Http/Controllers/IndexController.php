08524440010df70beb865d1017c0b576e02abf33
Maxim <maxim@MacBook-Pro-Maxim.local>
2020-04-28 16:54:48 +0600

    Images

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -126,6 +126,8 @@ class IndexController extends Controller
 
 		if (!in_array(str_replace("egg", "", $r->number), $lukkyArray)) {
 			$chance = 0;
+		} else {
+			$chance = 100;
 		}
 
 		$pro = mt_rand(1, 100);
