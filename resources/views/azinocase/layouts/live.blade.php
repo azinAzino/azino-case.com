3b10528aa61ddd45095dea86eb613f827d00f10c
MaxBrewer <pimax1978@icloud.com>
2020-05-05 18:05:57 +0600

    Lang

--- resources/views/azinocase/layouts/live.blade.php
+++ resources/views/azinocase/layouts/live.blade.php
@@ -13,7 +13,7 @@
 				$user = \DB::table('users')->where('id', $d->user)->first();
 				
 				$d->price = $item->cost;
-				$d->avatar = $user->avatar;
+				$d->avatar = "/img/avatar".SITE_ID.".png";
 			}
 		?>
         <div class="live-win__stat-wrapper">
