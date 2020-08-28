562d63f61b80f5c5c4ded09b2b1bde3497148ff8
MaxBrewer <pimax1978@icloud.com>
2020-08-28 21:40:31 +0600

    erer

--- resources/views/goldenmines/layouts/scripts.blade.php
+++ resources/views/goldenmines/layouts/scripts.blade.php
@@ -1,3 +1,6 @@
 <!--<a href="//showstreams.tv/"><img src="//www.free-kassa.ru/img/fk_btn/16.png" title="Бесплатный видеохостинг"></a>-->
+@php
+ $settings = App\Settings::where('id', SITE_ID)->first();   
+@endphp
 <script src="http://{{ $settings->url }}:3000/socket.io/socket.io.js"></script>
 <script data-cfasync="false" src="/cdn-cgi/scripts/af2821b0/cloudflare-static/email-decode.min.js"></script>
