46d04b34fead30e10b1fde18f4b203de275c57b5
MaxBrewer <pimax1978@icloud.com>
2020-08-30 15:39:28 +0600

    sdsd

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'tax_description' => "To credit funds to your account, you need to pay a winnings tax of :tax% to the Commission for the control of gambling. This fee is regulated by the Government of Malta in accordance with the legal provisions of the Malta Gambling Ordinance, which applies to the management and offer of online gambling services, including, but not limited to, casino, poker, sports betting and bingo betting."
+];
--- resources/views/goldenmines/finance/tax.blade.php
+++ resources/views/goldenmines/finance/tax.blade.php
@@ -2,7 +2,7 @@
     <div class="col-xs-12">
         <div class="commission-block text-center">
             <p><strong>{{ __('Dear player') }}! </strong></p>
-            <p>{{ __('To credit funds to your account, you need to pay a winnings tax of 13% to the Commission for the control of gambling. This fee is regulated by the Government of Malta in accordance with the legal provisions of the Malta Gambling Ordinance, which applies to the management and offer of online gambling services, including, but not limited to, casino, poker, sports betting and bingo betting.') }}</p>
+            <p>{{ __('messages.tax_description', ['tax' => $settings->tax]) }}</p>
             <div class="commission-block__btns">
                 <div class="text-center">
                     {{-- <span class="h4 text-center">{{ $tax_amount }}$</span>--}}
